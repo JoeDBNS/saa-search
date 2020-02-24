7ac3360aeee15f59b22f05050eeb2a1b249aaa33
Joe Davis <davisj38@michigan.gov>
2020-02-24 12:15:05 -0500

    various updates from ADO source repo

--- js/programs.js
+++ js/programs.js
@@ -32,12 +32,34 @@ A.length=len;
 // 20. Return A.
 return A}}()}
 
+// IE11 / Polyfill URLSearchParams() fix - https://github.com/jerrybendy/url-search-params-polyfill/blob/master/index.js
+(function(self){'use strict';var nativeURLSearchParams=(self.URLSearchParams&&self.URLSearchParams.prototype.get)?self.URLSearchParams:null,isSupportObjectConstructor=nativeURLSearchParams&&(new nativeURLSearchParams({a:1})).toString()==='a=1',decodesPlusesCorrectly=nativeURLSearchParams&&(new nativeURLSearchParams('s=%2B').get('s')==='+'),__URLSearchParams__="__URLSearchParams__",encodesAmpersandsCorrectly=nativeURLSearchParams?(function(){var ampersandTest=new nativeURLSearchParams();ampersandTest.append('s',' &');return ampersandTest.toString()==='s=+%26';})():true,prototype=URLSearchParamsPolyfill.prototype,iterable=!!(self.Symbol&&self.Symbol.iterator);if(nativeURLSearchParams&&isSupportObjectConstructor&&decodesPlusesCorrectly&&encodesAmpersandsCorrectly){return;} function URLSearchParamsPolyfill(search){search=search||"";if(search instanceof URLSearchParams||search instanceof URLSearchParamsPolyfill){search=search.toString();} this[__URLSearchParams__]=parseToDict(search);} prototype.append=function(name,value){appendTo(this[__URLSearchParams__],name,value);};prototype['delete']=function(name){delete this[__URLSearchParams__][name];};prototype.get=function(name){var dict=this[__URLSearchParams__];return name in dict?dict[name][0]:null;};prototype.getAll=function(name){var dict=this[__URLSearchParams__];return name in dict?dict[name].slice(0):[];};prototype.has=function(name){return name in this[__URLSearchParams__];};prototype.set=function set(name,value){this[__URLSearchParams__][name]=[''+value];};prototype.toString=function(){var dict=this[__URLSearchParams__],query=[],i,key,name,value;for(key in dict){name=encode(key);for(i=0,value=dict[key];i<value.length;i++){query.push(name+'='+encode(value[i]));}} return query.join('&');};var forSureUsePolyfill=!decodesPlusesCorrectly;var useProxy=(!forSureUsePolyfill&&nativeURLSearchParams&&!isSupportObjectConstructor&&self.Proxy);Object.defineProperty(self,'URLSearchParams',{value:(useProxy?new Proxy(nativeURLSearchParams,{construct:function(target,args){return new target((new URLSearchParamsPolyfill(args[0]).toString()));}}):URLSearchParamsPolyfill)});var USPProto=self.URLSearchParams.prototype;USPProto.polyfill=true;USPProto.forEach=USPProto.forEach||function(callback,thisArg){var dict=parseToDict(this.toString());Object.getOwnPropertyNames(dict).forEach(function(name){dict[name].forEach(function(value){callback.call(thisArg,value,name,this);},this);},this);};USPProto.sort=USPProto.sort||function(){var dict=parseToDict(this.toString()),keys=[],k,i,j;for(k in dict){keys.push(k);} keys.sort();for(i=0;i<keys.length;i++){this['delete'](keys[i]);} for(i=0;i<keys.length;i++){var key=keys[i],values=dict[key];for(j=0;j<values.length;j++){this.append(key,values[j]);}}};USPProto.keys=USPProto.keys||function(){var items=[];this.forEach(function(item,name){items.push(name);});return makeIterator(items);};USPProto.values=USPProto.values||function(){var items=[];this.forEach(function(item){items.push(item);});return makeIterator(items);};USPProto.entries=USPProto.entries||function(){var items=[];this.forEach(function(item,name){items.push([name,item]);});return makeIterator(items);};if(iterable){USPProto[self.Symbol.iterator]=USPProto[self.Symbol.iterator]||USPProto.entries;} function encode(str){var replace={'!':'%21',"'":'%27','(':'%28',')':'%29','~':'%7E','%20':'+','%00':'x00'};return encodeURIComponent(str).replace(/[!'()~]|%20|%00/g,function(match){return replace[match];});} function decode(str){return str.replace(/[ +]/g,'%20').replace(/(%[a-f0-9]{2})+/ig,function(match){return decodeURIComponent(match);});} function makeIterator(arr){var iterator={next:function(){var value=arr.shift();return{done:value===undefined,value:value};}};if(iterable){iterator[self.Symbol.iterator]=function(){return iterator;};} return iterator;} function parseToDict(search){var dict={};if(typeof search==="object"){if(isArray(search)){for(var i=0;i<search.length;i++){var item=search[i];if(isArray(item)&&item.length===2){appendTo(dict,item[0],item[1]);}else{throw new TypeError("Failed to construct 'URLSearchParams': Sequence initializer must only contain pair elements");}}}else{for(var key in search){if(search.hasOwnProperty(key)){appendTo(dict,key,search[key]);}}}}else{if(search.indexOf("?")===0){search=search.slice(1);} var pairs=search.split("&");for(var j=0;j<pairs.length;j++){var value=pairs[j],index=value.indexOf('=');if(-1<index){appendTo(dict,decode(value.slice(0,index)),decode(value.slice(index+1)));}else{if(value){appendTo(dict,decode(value),'');}}}} return dict;} function appendTo(dict,name,value){var val=typeof value==='string'?value:(value!==null&&value!==undefined&&typeof value.toString==='function'?value.toString():JSON.stringify(value));if(name in dict){dict[name].push(val);}else{dict[name]=[val];}} function isArray(val){return!!val&&'[object Array]'===Object.prototype.toString.call(val);}})(typeof global!=='undefined'?global:(typeof window!=='undefined'?window:this));
+
 
 window.addEventListener('load', function() {
+  GetUrlParams();
   SetMenuListener();
-  // MainVue.GetMiTalentPrograms();
+
+  if (typeof MainVue.urlParams.programSearch !== 'undefined') {
+    MainVue.search = MainVue.urlParams.programSearch;
+    MainVue.GetMiTalentProgramsUnique();
+  }
 });
 
+window.onbeforeunload = function() {
+  window.history.replaceState(null, null, '?programSearch=' + encodeURI(MainVue.search.trim().replace(/\./g, '').replace(/\'/g, '')));
+};
+
+function GetUrlParams() {
+	var urlParams = new URLSearchParams(window.location.search);
+	var entries = urlParams.entries();
+	var entriesDict = {};
+	var entriesArray = Array.from(entries);
+	entriesArray.forEach(function(entry) {
+		entriesDict[entry[0]] = entry[1];
+	});
+	MainVue.urlParams = entriesDict;
+}
 
 // Create event listener for menu element open and close
 function SetMenuListener() {
@@ -200,6 +222,10 @@ var MainVue = new Vue({
   watch: {
     results: function() {
       MainVue.FilterResults();
+
+      if (MainVue.results.length === 1) {
+        MainVue.SelectResult(MainVue.results[0]);
+      }
     },
     filter_type: function() {
       MainVue.FilterResults();
